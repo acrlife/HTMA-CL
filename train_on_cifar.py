b1964bcbbe0e026cd067dcdc87a214b7bc50f207
JYH <39241836+acrlife@users.noreply.github.com>
2024-12-07 15:20:47 +0800

    Update train_on_cifar.py

--- train_on_cifar.py
+++ train_on_cifar.py
@@ -72,15 +72,14 @@ def main():
     # Data
     print('==> Preparing data..')
     transform_train = transforms.Compose([
-        transforms.Resize(args.img_size),
-        transforms.RandomCrop(args.img_size, padding=(args.img_size//8)),
+        transforms.Resize(32),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
     transform_test = transforms.Compose([
-        transforms.Resize(args.img_size),
+        transforms.Resize(32),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
